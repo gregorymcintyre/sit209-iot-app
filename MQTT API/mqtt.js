42c75e3011ed1bd577e9866da242d7b32797b1e7
seanpain4 <seanpain4@gmail.com>
2019-09-02 22:51:22 +1000

    ADD: Add MQTT Sub

---
+++ MQTT API/mqtt.js
@@ -0,0 +1,15 @@
+const mqtt = require('mqtt');
+
+const client = mqtt.connect("mqtt://soldier.cloudmqtt.com:15615", {
+  username: "qhwjeaqg", 
+  password: "Y1rsufr-Ij8i"
+});
+
+client.on('connect', () => {
+  client.subscribe('/#');
+  console.log('connected');
+});
+
+client.on('message', (topic, message) => {
+  console.log(`Received message on ${topic}: ${message}`);
+});
