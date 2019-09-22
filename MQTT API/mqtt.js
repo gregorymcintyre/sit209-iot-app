74eaed19e9d41a8bb37a714034b5fb1e0add0e55
seanpain4 <seanpain4@gmail.com>
2019-09-22 22:13:35 +1000

    ENHANCE: Docs and MQTT Backend

--- MQTT API/mqtt.js
+++ MQTT API/mqtt.js
@@ -1,30 +1,30 @@
-/*
-https://sanctuaries.noaa.gov/visit/ecosystems/kelpdesc.html
-
-if (temp < 5 || light < 1){				//if its too cold or the light is too low
-	if (pressure >= .2){				//and the pressure is low enough to move up
-		bouyancy++;				//move up
-	}else{
-	//report no ideal conditions (GUI)		//send status alert
-	}
-}
-
-if (temp > 20 || light > 1){				//if its too warm or the light is too much
-	if (pressure <= 3){				//and the pressure is not too high to move down
-		bouyancy--;				//move down
-	}else{
-	//report non ideal conditions (GUI)		//send status alert
-	}
-}
-*/
-
-
 const mqtt = require('mqtt');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Device = require('./models/device');
 const rand = require('random-int');
+
+// Process environmenal values
+function processSensorData(input) {
+  const {li, pres, temp} = input.sensorData;
+  
+  if (temp < 5 || li < 1) {   // If it is too cold or the light is too low.
+    if (pres >= .2) {   // And the pressure is low enough to move up.
+      // Send command to move up.
+    } else {
+      // Report no ideal conditions to GUI.
+    }
+  }
+  
+  if (temp > 20 || li > 1) {   // If it is too warm or the light is too much.
+    if (pres <= 3) {   // And the pressure is not too high to move down.
+      // Send command to move down.
+    } else {
+      // Report non ideal conditions to GUI.
+    }
+  }
+}
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -33,7 +33,7 @@ const client = mqtt.connect("mqtt://soldier.cloudmqtt.com:15615", {
   password: "Y1rsufr-Ij8i"
 });
 
-const approved_devices = ['arduino_1','arduino_test'];
+const approved_devices = ['arduino_test'];
 const app = express();
 const port = 5001;
 
@@ -50,21 +50,21 @@ client.on('connect', () => {
 });
 
 // Logging data to mongoDB
-
 client.on('message', (topic, message) => {
-  console.log(`Received message on ${topic}: ${message}`);
   var topic_split = topic.split("/");
   if (approved_devices.indexOf(topic_split[0]) >= 0) {
     if (topic_split[1] == 'sd') {
+      console.log(`Received message on ${topic}: ${message}`);
       const data = JSON.parse(message);
+      processSensorData(data);
       Device.findOne({"deviceId": data.deviceId }, (err, device) => {
         if (err) {
           console.log(err)
         } else if (!device) {
           console.log("Creating new device...");
           const {deviceId} = data;
-          const {ts, loc, li, pres, bouy} = data.sensorData;
-          const sensorData = {ts, loc, li, pres, bouy};
+          const {ts, temp, li, pres, bouy} = data.sensorData;
+          const sensorData = {ts, temp, li, pres, bouy};
           const newDevice = new Device({
             deviceId,
             sensorData
@@ -77,9 +77,9 @@ client.on('message', (topic, message) => {
           });
         } else {
           const { sensorData } = device;
-          const { ts, loc, li, pres, bouy } = data.sensorData;
+          const { ts, temp, li, pres, bouy } = data.sensorData;
         
-          sensorData.push({ ts, loc, li, pres, bouy });
+          sensorData.push({ ts, temp, li, pres, bouy });
           device.sensorData = sensorData;
         
           device.save(err => {
@@ -89,27 +89,20 @@ client.on('message', (topic, message) => {
           });
         }
       });
-    } else {
-      console.log("Unkown topic!");
     }
-  } else {
-    console.log("Device not approved!");
   }
 });
 
-
-
 // TEST MESSAGE CODE
-
 app.put('/sensor-data', (req, res) => {
   const { deviceId } = req.body;
   const ts = new Date().getTime();
-  const loc = "Brisbane";
-  const light = rand(0, 50);
-  const pressure = rand(0, 50);
-  const bouyancy = rand(0, 50);
-  sensorData = {ts, loc, light, pressure, bouyancy}
-  const topic = `${deviceId}/sensorData`;
+  const temp = 23;
+  const li = rand(0, 50);
+  const pres = rand(0, 50);
+  const bouy = rand(0, 50);
+  sensorData = {ts, temp, li, pres, bouy}
+  const topic = `${deviceId}/sd`;
   const message = JSON.stringify({ deviceId, sensorData});
   client.publish(topic, message, () => {
     res.send('published new message');
